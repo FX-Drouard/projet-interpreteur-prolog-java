f2ea7f28b446997b35c80d5a7510546181bc7398
palis <palis@LAPTOP-AI95NBJO>
2022-02-22 13:56:51 +0100

    quelques règles ajoutées, pas testées

--- java/src/pcomp/prolog/ast/Equation.java
+++ java/src/pcomp/prolog/ast/Equation.java
@@ -1,5 +1,7 @@
 package pcomp.prolog.ast;
 
+import java.util.List;
+
 public class Equation {
 	
 	private Term gauche, droite;
@@ -7,6 +9,48 @@ public class Equation {
 	public Equation(Term g, Term d) {
 		gauche = g;
 		droite = d;
+	}
+	
+	public boolean effacer(Systeme s) {
+		if (gauche.equals(droite)) {
+			s.removeEquation(this);
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean orienter(Systeme s) {
+		if (droite instanceof TermVariable && gauche instanceof TermPredicate) {
+			Equation nouv = new Equation(droite, gauche);
+			s.addEquation(nouv);
+			s.removeEquation(this);
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean decomposer(Systeme s) {
+		//comparaison des symboles si on a des prédicats à gauche et à droite
+		if (gauche instanceof TermPredicate && droite instanceof TermPredicate) {
+			Predicate p1 = ((TermPredicate)gauche).getPredicate();
+			Predicate p2 = ((TermPredicate)droite).getPredicate();
+			if (p1.getSymbol().equals(p2.getSymbol())) {
+				// Décomposition possible!
+				List<Term> args1 = p1.getArguments();
+				List<Term> args2 = p2.getArguments();
+				for (int i = 0;i<args1.size();i++) {
+					s.addEquation(new Equation(args1.get(i),args2.get(i)));
+				}
+				s.removeEquation(this);
+				return true;
+			}
+			System.out.println("Cas où les symboles des prédicats non-compatibles : on arrête tout?");
+		}
+		return false;
+	}
+	
+	public boolean remplacer(Systeme s, Equation e) {
+		return false;
 	}
 	
 	public String toString() {
--- java/src/pcomp/prolog/ast/Systeme.java
+++ java/src/pcomp/prolog/ast/Systeme.java
@@ -7,20 +7,36 @@ public class Systeme {
 	
 	private List<Equation> eqs = new ArrayList<>();
 	
-	public void effacer() {
-		
+	public void addEquation(Equation e) {
+		eqs.add(e);
 	}
 	
-	public void orienter() {
-		
+	public void removeEquation(Equation e) {
+		eqs.remove(e);
 	}
 	
-	public void decomposer() {
-		
+	//Renvoie une copie en surface de la liste eqs
+	private List<Equation> copie() {
+		List<Equation> res = new ArrayList<>();
+		for (Equation e : eqs) {
+			res.add(e);
+		}
+		return res;
 	}
 	
-	public void remplacer() {
-		
+	public void unification() {
+		// regleapp sert de condition d'arrêt de notre boucle d'unification
+		boolean regleapp = true;
+		// liste contiendra le nouveau système issu d'une boucle dans l'unification
+		List<Equation> nouvSys;
+		while (regleapp) {
+			regleapp = false; //aucune règle n'a été appliquée sur le système pendant ce tour
+			nouvSys = copie();
+			
+			// Application des règles
+			
+			eqs = nouvSys;
+		}
 	}
 
 }
