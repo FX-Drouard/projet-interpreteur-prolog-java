b9c528a29553c952fd0e475fc156aa735b03c536
FX  Drouard <3800028>
2022-02-27 10:40:04 +0100

    fin test

--- java/src/pcomp/prolog/ast/test/MainUnification.java
+++ java/src/pcomp/prolog/ast/test/MainUnification.java
@@ -11,6 +11,7 @@ public class MainUnification {
 	public static void main(String[] args) {
 		// Tests avec les équations du TD
 		
+		System.out.println("\n\n\n Eq 1\n\n");
 		// 1 :
 		Program p1 = PrologParser.parseString("?- p(Z,h(Z,W),f(W)).");
 		System.out.println(p1);
@@ -27,6 +28,7 @@ public class MainUnification {
 		s1.afficherSysteme();
 		s1.unify();
 		
+		System.out.println("\n\n\n Eq 2\n\n");
 		// 2 :
 		Program p21 = PrologParser.parseString("?- p(a,X,f(g(Y))).");
 		System.out.println(p21);
@@ -43,7 +45,8 @@ public class MainUnification {
 		s2.afficherSysteme();
 		s2.unify();
 		
-		// 2 :
+		System.out.println("\n\n\n Eq 3\n\n");
+		// 3 :
 		Program p31 = PrologParser.parseString("?- q(f(a),g(X)).");
 		System.out.println(p31);
 		Program p32 = PrologParser.parseString("?- q(Y,Y).");
@@ -58,6 +61,110 @@ public class MainUnification {
 		s3.afficherEnv();
 		s3.afficherSysteme();
 		s3.unify();
+		
+		System.out.println("\n\n\n Eq 4\n\n");
+		// 4 : 
+		Program p41 = PrologParser.parseString("?- p(x,f(x),f(f(x))).");
+		System.out.println(p41);
+		Program p42 = PrologParser.parseString("?- p(f(f(y)),y,f(y)).");
+		System.out.println(p42);
+		Predicate pred41 = p41.getDeclarations().get(0).getPredicates().get(0);
+		System.out.println(pred41);
+		Predicate pred42 = p42.getDeclarations().get(0).getPredicates().get(0);
+		System.out.println(pred42);
+		Equation eq4 = new Equation(new TermPredicate(pred41,pred41.getPosition()), new TermPredicate(pred42,pred42.getPosition()));
+		Systeme s4 = new Systeme();
+		s4.addEquation(eq4);
+		s4.afficherEnv();
+		s4.afficherSysteme();
+		s4.unify();	
+		
+		
+		System.out.println("\n\n\n Eq 5\n\n");
+		// 5 : 
+		Program p51 = PrologParser.parseString("?- q(x, q(y, z)).");
+		System.out.println(p51);
+		Program p52 = PrologParser.parseString("?- q(x, g(h(k(x)))).");
+		System.out.println(p52);
+		Predicate pred51 = p51.getDeclarations().get(0).getPredicates().get(0);
+		System.out.println(pred51);
+		Predicate pred52 = p52.getDeclarations().get(0).getPredicates().get(0);
+		System.out.println(pred52);
+		Equation eq5 = new Equation(new TermPredicate(pred51,pred51.getPosition()), new TermPredicate(pred52,pred52.getPosition()));
+		Systeme s5 = new Systeme();
+		s5.addEquation(eq5);
+		s5.afficherEnv();
+		s5.afficherSysteme();
+		s5.unify();	
+		
+		System.out.println("\n\n\n Eq 6\n\n");
+		// 6 : 
+		Program p61 = PrologParser.parseString("?-  q(x, r(u, x)).");
+		System.out.println(p61);
+		Program p62 = PrologParser.parseString("?-  q(r(y, a), r(z, r(b, z))).");
+		System.out.println(p62);
+		Predicate pred61 = p61.getDeclarations().get(0).getPredicates().get(0);
+		System.out.println(pred61);
+		Predicate pred62 = p62.getDeclarations().get(0).getPredicates().get(0);
+		System.out.println(pred62);
+		Equation eq6 = new Equation(new TermPredicate(pred61,pred61.getPosition()), new TermPredicate(pred62,pred62.getPosition()));
+		Systeme s6 = new Systeme();
+		s6.addEquation(eq6);
+		s6.afficherEnv();
+		s6.afficherSysteme();
+		s6.unify();	
+				
+		System.out.println("\n\n\n Eq 7\n\n");
+		// 7 : 
+		Program p71 = PrologParser.parseString("?-  p(x, f(x), r(f(x), x)).");
+		System.out.println(p71);
+		Program p72 = PrologParser.parseString("?-  p(z, f(f(a)), r(f(r(a, z)), v)).");
+		System.out.println(p72);
+		Predicate pred71 = p71.getDeclarations().get(0).getPredicates().get(0);
+		System.out.println(pred71);
+		Predicate pred72 = p72.getDeclarations().get(0).getPredicates().get(0);
+		System.out.println(pred72);
+		Equation eq7 = new Equation(new TermPredicate(pred71,pred71.getPosition()), new TermPredicate(pred72,pred72.getPosition()));
+		Systeme s7 = new Systeme();
+		s7.addEquation(eq7);
+		s7.afficherEnv();
+		s7.afficherSysteme();
+		s7.unify();	
+				
+			
+		System.out.println("\n\n\n Eq 8\n\n");
+		// 8 : 
+		Program p81 = PrologParser.parseString("?-  p(f(r(x, y)), r(v, w), y).");
+		System.out.println(p81);
+		Program p82 = PrologParser.parseString("?-  p(f(z), x, f(x)).");
+		System.out.println(p82);
+		Predicate pred81 = p81.getDeclarations().get(0).getPredicates().get(0);
+		System.out.println(pred81);
+		Predicate pred82 = p82.getDeclarations().get(0).getPredicates().get(0);
+		System.out.println(pred82);
+		Equation eq8 = new Equation(new TermPredicate(pred81,pred81.getPosition()), new TermPredicate(pred82,pred82.getPosition()));
+		Systeme s8 = new Systeme();
+		s8.addEquation(eq8);
+		s8.afficherEnv();
+		s8.afficherSysteme();
+		s8.unify();	
+		
+		System.out.println("\n\n\n Eq 9\n\n");
+		// 9 : 
+		Program p91 = PrologParser.parseString("?-  p(f(y), f(z), f(t), f(x)).");
+		System.out.println(p91);
+		Program p92 = PrologParser.parseString("?-  p(g(z), g(x), g(y), g(z)).");
+		System.out.println(p92);
+		Predicate pred91 = p91.getDeclarations().get(0).getPredicates().get(0);
+		System.out.println(pred91);
+		Predicate pred92 = p92.getDeclarations().get(0).getPredicates().get(0);
+		System.out.println(pred92);
+		Equation eq9 = new Equation(new TermPredicate(pred91,pred91.getPosition()), new TermPredicate(pred92,pred92.getPosition()));
+		Systeme s9 = new Systeme();
+		s9.addEquation(eq9);
+		s9.afficherEnv();
+		s9.afficherSysteme();
+		s9.unify();	
 	}
 
 }
