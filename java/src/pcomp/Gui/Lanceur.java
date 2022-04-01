58b7a646706d167eb4e00991b6188c739724626e
fxhbb <fxhbb@PC-Gamer-Flow.home>
2022-04-01 14:42:45 +0200

    branchement interprete gui

--- java/src/pcomp/Gui/Lanceur.java
+++ java/src/pcomp/Gui/Lanceur.java
@@ -1,5 +1,13 @@
 package pcomp.Gui;
 import pcomp.*;
+import pcomp.prolog.ast.CurrContext;
+import pcomp.prolog.ast.Decl;
+import pcomp.prolog.ast.Environnement;
+import pcomp.prolog.ast.Interprete;
+import pcomp.prolog.ast.Program;
+import pcomp.prolog.ast.VisitorDecl;
+import pcomp.prolog.ast.excep.NoSolutionException;
+import pcomp.prolog.parser.PrologParser;
 
 public class Lanceur {
 	private final String textoriginel;
@@ -9,8 +17,19 @@ public class Lanceur {
 	}
 	
 	public String run() {
+		StringBuilder res= new StringBuilder();
 		try{
-			return "TODO";
+			//test
+			Program prog = PrologParser.parseString(textoriginel);
+			res.append(prog);
+			res.append("\nSolution: ");
+			try {
+				Environnement env = Interprete.interprete4(prog);
+				res.append(env);
+			} catch (NoSolutionException excep) {
+				res.append("Jalon 5 "+excep.getMessage());
+			}
+			return res.toString();
 		}catch (Exception e) {
 			Question.warn("Une erreur à été detecté!");
 			return "Erreur Critique! "+e.getMessage();
